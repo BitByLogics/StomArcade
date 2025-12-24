67729d0404b24833f564019c77acb5cf04835b93
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-12-23 23:44:04 -0500

    Add Polar world loading and player spawning configuration

--- src/main/java/net/bitbylogic/stomarcade/StomArcadeServer.java
+++ src/main/java/net/bitbylogic/stomarcade/StomArcadeServer.java
@@ -1,7 +1,15 @@
 package net.bitbylogic.stomarcade;
 
+import net.hollowcube.polar.PolarLoader;
 import net.minestom.server.Auth;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.SharedInstance;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class StomArcadeServer {
 
@@ -15,6 +23,28 @@ public class StomArcadeServer {
         MinecraftServer.setCompressionThreshold(0);
 
         MinecraftServer minecraftServer = MinecraftServer.init(new Auth.Velocity(velocitySecret));
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        String worldName = System.getenv("WORLD_NAME");
+        Path worldPath = Path.of(String.format("./%s.polar", worldName));
+
+        if (!worldPath.toFile().exists()) {
+            throw new RuntimeException("World file does not exist");
+        }
+
+        try {
+            instanceContainer.setChunkLoader(new PolarLoader(worldPath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        SharedInstance sharedInstance = instanceManager.createSharedInstance(instanceContainer);
+
+        MinecraftServer.getGlobalEventHandler().addListener(AsyncPlayerConfigurationEvent.class, event -> {
+           event.setSpawningInstance(sharedInstance);
+        });
 
         String serverAddress = System.getenv("SERVER_ADDRESS");
 
