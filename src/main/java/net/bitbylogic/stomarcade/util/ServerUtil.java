1636d21590c07e970395b64a4ef833ec338e8b49
Justin <33465177+BitByLogics@users.noreply.github.com>
2025-12-30 16:29:54 -0500

    Add `DefaultFontInfo` enum for character length definitions and `center` method for text alignment

--- src/main/java/net/bitbylogic/stomarcade/util/ServerUtil.java
+++ src/main/java/net/bitbylogic/stomarcade/util/ServerUtil.java
@@ -1,9 +1,35 @@
 package net.bitbylogic.stomarcade.util;
 
+import net.bitbylogic.stomarcade.util.message.DefaultFontInfo;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Random;
 
 public class ServerUtil {
 
     public static final Random RANDOM = new Random();
+
+    private static final int MOTD_CENTER_PX = 125;
+
+    public static Component center(@NotNull Component component) {
+        String plain = MiniMessage.miniMessage().serialize(component.style(Style.style().build()));
+
+        int messagePxSize = 0;
+
+        for (char c : plain.toCharArray()) {
+            messagePxSize += DefaultFontInfo.getDefaultFontInfo(c).getLength() + 1;
+        }
+
+        int halvedMessageSize = messagePxSize / 2;
+        int toCompensate = MOTD_CENTER_PX - halvedMessageSize;
+
+        int spacePx = DefaultFontInfo.getDefaultFontInfo(' ').getLength();
+        int compensatedSpaces = toCompensate / (spacePx + 1);
+
+        return Component.text(" ".repeat(Math.max(0, compensatedSpaces))).append(component);
+    }
 
 }
