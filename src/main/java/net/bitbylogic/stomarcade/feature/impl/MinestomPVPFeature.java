87122d2a5c533a6e25dbe0c1b865a3552c2e670c
Justin <33465177+BitByLogics@users.noreply.github.com>
2026-01-05 20:38:22 -0500

    Refactor `MinestomPVPFeature` to reuse event node instance for enable/disable lifecycle

--- src/main/java/net/bitbylogic/stomarcade/feature/impl/MinestomPVPFeature.java
+++ src/main/java/net/bitbylogic/stomarcade/feature/impl/MinestomPVPFeature.java
@@ -5,32 +5,37 @@ import io.github.togar2.pvp.feature.CombatFeatures;
 import io.github.togar2.pvp.feature.config.DefinedFeature;
 import net.bitbylogic.stomarcade.feature.SimpleFeature;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.trait.EntityInstanceEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class MinestomPVPFeature extends SimpleFeature {
 
     private final CombatFeatureSet featureSet;
+    private final EventNode<EntityInstanceEvent> node;
 
     public MinestomPVPFeature(@NotNull String id, @NotNull DefinedFeature<?> feature) {
         super(id);
 
-        featureSet = CombatFeatures.empty().add(feature).build();
+        this.featureSet = CombatFeatures.empty().add(feature).build();
+        this.node = featureSet.createNode();
     }
 
     public MinestomPVPFeature(@NotNull String id, @NotNull CombatFeatureSet featureSet) {
         super(id);
 
         this.featureSet = featureSet;
+        this.node = featureSet.createNode();
     }
 
     @Override
     public void onEnable() {
-        MinecraftServer.getGlobalEventHandler().addChild(featureSet.createNode());
+        MinecraftServer.getGlobalEventHandler().addChild(node);
     }
 
     @Override
     public void onDisable() {
-        MinecraftServer.getGlobalEventHandler().removeChild(featureSet.createNode());
+        MinecraftServer.getGlobalEventHandler().removeChild(node);
     }
 
     protected CombatFeatureSet getFeatureSet() {
